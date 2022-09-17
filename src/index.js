ce63db3790382b4afbc81d944eeafdfe452f906d
Ghost1706 <56081021+Ghost1706@users.noreply.github.com>
2022-09-17 21:47:41 +0100

    NavConfig hotfix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import NavConfig from "./navigation/navconfig";
+import NavConfig from "./navigation/NavConfig";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
