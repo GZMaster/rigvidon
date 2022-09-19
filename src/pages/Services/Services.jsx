a0d761af399fee08860726ad912bd26d7e4098db
GZMaster <danigamester@gmail.com>
2022-09-19 16:40:05 +0100

    services right side design

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -11,18 +11,35 @@ const Services = () => {
     <section id="services" className="S_Container">
       <div className="S_Wrapper">
         <div className="S_Left">
+          
+        </div>
+
+        <div className="S_Right">
           <div className="S_Top">
             <h2>OUR SERVICES</h2>
           </div>
+
           <p className="S_Wrapper_Text">
             Below are some of the services we offer to our clients
           </p>
+
+          <div className="servives_holder">
+            <button className="S_Left_Button" onClick={() => sideScroll(document.getElementById('services_card'),'left',25,200,10)}><AiOutlineArrowLeft /></button>
+            <div className="services_card" id="services_card">
+              <Card items={cardData} />
+            </div>
+            <button className="S_Right_Button" onClick={() => sideScroll(document.getElementById('services_card'),'right',25,200,10)}><AiOutlineArrowRight /></button>
+          </div>
+
+          <div class="pagination">
+            <span class="active"></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
-        <button className="S_Left_Button" onClick={() => sideScroll(document.getElementById('services_card'),'left',25,100,10)}><AiOutlineArrowLeft /></button>
-        <div className="S_Right" id="services_card">
-            <Card className="card_items" items={cardData} />
-        </div>
-        <button className="S_Right_Button" onClick={() => sideScroll(document.getElementById('services_card'),'right',25,100,10)}><AiOutlineArrowRight /></button>
       </div>
     </section>
   );
